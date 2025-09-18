55fb961fb40aff3f9cd59a489fd6b6779f0a3f5d
Reinaldo <reinaldo18junior@gmail.com>
2025-09-18 14:36:46 -0300

    Ribbon load balancing

--- hr-payroll/src/main/java/com/example/hrpayroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/com/example/hrpayroll/HrPayrollApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@RibbonClient(name = "hr-worker")
 @EnableFeignClients
 @SpringBootApplication
 public class HrPayrollApplication {
