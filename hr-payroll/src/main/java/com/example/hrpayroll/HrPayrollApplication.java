acb384a3c6b4b006f04db4033b188fcdb28d6dd3
Reinaldo <reinaldo18junior@gmail.com>
2025-09-19 13:59:11 -0300

    Configuring Eureka clients

--- hr-payroll/src/main/java/com/example/hrpayroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/com/example/hrpayroll/HrPayrollApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@RibbonClient(name = "hr-worker")
+@EnableEurekaClient
 @EnableFeignClients
 @SpringBootApplication
 public class HrPayrollApplication {
--- hr-worker/src/main/java/com/example/hrworker/HrWorkerApplication.java
+++ hr-worker/src/main/java/com/example/hrworker/HrWorkerApplication.java
@@ -3,6 +3,7 @@ package com.example.hrworker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class HrWorkerApplication {
 
